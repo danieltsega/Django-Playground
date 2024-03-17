9c855165748773f488ca39c16a5f83452480069d
danny <danny@DannyX.myguest.virtualbox.org>
2024-03-17 14:17:27 +0300

    [Feature]: Customized the django form input field using tailwind

--- Todo/todo/forms.py
+++ Todo/todo/forms.py
@@ -3,6 +3,10 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    task = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'h-10 w-full border rounded focus:outline-none py-3 px-3 text-gray-700  mr-2',
+        'placeholder': 'Enter your task here...'
+    }), label='')
     class Meta:
         model = Task
         fields = ['task']
