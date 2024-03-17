8cf3861a91e36ccb5aee20d8232b218d78498f8b
danny <danny@DannyX.myguest.virtualbox.org>
2024-03-17 10:10:30 +0300

    [Feature]: Adding the urls to handle crud operation

--- Todo/todo/urls.py
+++ Todo/todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, delete_task, completed
 
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+    #path("add-task/", add_task, name="add-task"),
+    path("delete-task/<int:task_id>/", delete_task, name="delete-task"),
+    path("completed/<int:task_id>", completed, name="completed")
 ]
