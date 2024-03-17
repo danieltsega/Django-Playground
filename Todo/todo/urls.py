de74cd894035217c41f5ff84d766b0694973a2fa
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-17 16:09:51 +0300

    [Feature]: Adding the edit form url

--- Todo/todo/urls.py
+++ Todo/todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, delete_task, completed
+from .views import index, delete_task, completed, edit_task
 
 urlpatterns = [
     path("", index, name="home"),
     #path("add-task/", add_task, name="add-task"),
     path("delete-task/<int:task_id>/", delete_task, name="delete-task"),
-    path("completed/<int:task_id>", completed, name="completed")
+    path("completed/<int:task_id>", completed, name="completed"),
+    path("edit-task/<int:task_id>/", edit_task, name="edit-task")
 ]
