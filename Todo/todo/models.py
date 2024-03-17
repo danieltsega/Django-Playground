7f520d2e775f75e41ca3853be9a484e1b2e5d409
danny <danny@DannyX.myguest.virtualbox.org>
2024-03-17 12:14:12 +0300

    [Fix]: Fixed the done field

--- Todo/todo/models.py
+++ Todo/todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     task = models.CharField(max_length=200)
-    done = models.IntegerField(default=0)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.task
