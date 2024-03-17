18e9bad1076c5776854850c5857e6a9f6a51fa93
danny <danny@DannyX.myguest.virtualbox.org>
2024-03-17 10:09:56 +0300

    [Feature]: Adding the task model

--- Todo/todo/models.py
+++ Todo/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    done = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.task
