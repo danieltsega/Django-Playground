87be69002f0350194dafa5b3f1fd290050696dbb
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-17 14:20:17 +0300

    [Fix]: Fixed the filter view method by geting the undone tasks to the template

--- Todo/todo/views.py
+++ Todo/todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 from .models import Task
 
@@ -6,7 +7,7 @@ from .forms import TaskForm
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(done=0)
     form = TaskForm()
     if request.method == "POST":
         form = TaskForm(request.POST)
@@ -17,9 +18,10 @@ def index(request):
     tasks_done = Task.objects.filter(done=1)
     return render(request, 'index.html', {"tasks": tasks, "form": form, "tasks_done": tasks_done})
 
+@require_POST
 def completed(request, task_id):
     task = Task.objects.get(id=task_id)
-    task.objects.update(done=1)
+    task.done = not task.done  # Toggle the done status
     task.save()
     return redirect('home')
 
