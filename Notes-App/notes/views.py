a083d1f216073358632d29c3059174bfb519cb11
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-19 15:01:01 +0300

    [Feature]: Add the class based view of create view to add a note

--- Notes-App/notes/views.py
+++ Notes-App/notes/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from .models import Note
+from .forms import NoteForm
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html", {})
+
+class CreateNoteView(CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "add_note.html"
+    success_url = reverse_lazy('home')
