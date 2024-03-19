912e24b88cd62701b74d05d8343f48ffd4351a92
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-19 14:59:23 +0300

    [Feature]: Adding and customizing the django form

---
+++ Notes-App/notes/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import Note
+
+class NoteForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'h-10 w-full border rounded focus:outline-none py-3 px-3 text-gray-700  mr-2 bg-slate-200',
+        'placeholder': 'Add your note title here...'
+    }), label='')
+    description = forms.TimeField(widget=forms.Textarea(attrs={
+        'class': 'h-64 w-full border rounded focus:outline-none py-3 px-3 text-gray-700 mt-2 bg-slate-200',
+        'placeholder': 'Write your note here...'
+    }), label='')
+    
+    class Meta:
+        model = Note
+        fields = ['title', 'description']
