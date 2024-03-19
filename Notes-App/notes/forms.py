bd2a3d3390f5dd0659de8b87325d6386a689e8e3
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-19 18:56:10 +0300

    [Fix]: Fixed the input type of the description

--- Notes-App/notes/forms.py
+++ Notes-App/notes/forms.py
@@ -7,7 +7,7 @@ class NoteForm(forms.ModelForm):
         'class': 'h-10 w-full border rounded focus:outline-none py-3 px-3 text-gray-700  mr-2 bg-slate-200',
         'placeholder': 'Add your note title here...'
     }), label='')
-    description = forms.TimeField(widget=forms.Textarea(attrs={
+    description = forms.CharField(widget=forms.Textarea(attrs={
         'class': 'h-64 w-full border rounded focus:outline-none py-3 px-3 text-gray-700 mt-2 bg-slate-200',
         'placeholder': 'Write your note here...'
     }), label='')
