043c962fa066b550e87c191d3bd61e404df5cb9a
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-19 15:00:22 +0300

    [Feature]: Adding the add note url

--- Notes-App/notes/urls.py
+++ Notes-App/notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, CreateNoteView
 
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+    path("add-note", CreateNoteView.as_view(), name="add-note")
 ]
