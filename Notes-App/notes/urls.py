9ee07f2f0b802d99b8a309de7aa4c8796a1484d5
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-19 19:25:43 +0300

    [Feature]: Add the delete and update urls

--- Notes-App/notes/urls.py
+++ Notes-App/notes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ListNoteView, CreateNoteView
+from .views import ListNoteView, CreateNoteView, DeleteNoteView, UpdateNoteView
 
 urlpatterns = [
     path("", ListNoteView.as_view(), name="home"),
-    path("add-note", CreateNoteView.as_view(), name="add-note")
+    path("add-note", CreateNoteView.as_view(), name="add-note"),
+    path("delete-note/<int:pk>/", DeleteNoteView.as_view(), name="delete-note"),
+    path("edit-note/<int:pk>", UpdateNoteView.as_view(), name="edit-note")
 ]
