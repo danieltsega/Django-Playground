623aa1ed1806d378bf878b8821d5c292b037a694
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-19 18:56:41 +0300

    [Feature]: Adding the class based list view url

--- Notes-App/notes/urls.py
+++ Notes-App/notes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import index, CreateNoteView
+from .views import ListNoteView, CreateNoteView
 
 urlpatterns = [
-    path("", index, name="home"),
+    path("", ListNoteView.as_view(), name="home"),
     path("add-note", CreateNoteView.as_view(), name="add-note")
 ]
