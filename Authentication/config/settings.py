7f6805c47e6e785827b18505743a952e0bdc772a
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-21 11:02:02 +0300

    [Feature]: Add the login redirect and logout redirect in the settings

--- Authentication/config/settings.py
+++ Authentication/config/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
