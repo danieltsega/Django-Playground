d8306918b6ebb55f2c6b46a3cd8fc257f9f6193e
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-21 11:00:38 +0300

    [Feature]: Add the user login and signup url

--- Authentication/config/urls.py
+++ Authentication/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name="signup")
 ]
