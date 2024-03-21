1dc8fb2fe77ee69757e3283de677309dfbaa9234
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-21 18:10:14 +0300

    [Feature]: Added the login, logout and signup urls

--- Authentication/config/urls.py
+++ Authentication/config/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', SignUpView.as_view(), name="signup")
+    #path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', SignUpView.as_view(), name="signup"),
+    path('accounts/login/', CustomLoginView.as_view(), name="login"),
+    path('accounts/logout/', LogoutView.as_view(), name="logout")
 ]
