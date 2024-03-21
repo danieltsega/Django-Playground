fa891c4ced21ae84ecd6c8ce3244306251bc43be
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-21 18:10:38 +0300

    [Feature]: Add the custom login and signup views

--- Authentication/config/views.py
+++ Authentication/config/views.py
@@ -1,8 +1,14 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from base.forms import CustomUserForm, CustomLoginForm
+from django.contrib.auth.views import LoginView
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+    
+class CustomLoginView(LoginView):
+    form_class = CustomLoginForm
+    template_name = 'registration/login.html'
