3dd4346ab453e0c73cfd3d4e30f7727ad2027008
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-21 11:15:38 +0300

    [Feature]: Add the login url in the settings

--- Authentication/config/settings.py
+++ Authentication/config/settings.py
@@ -129,3 +129,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
