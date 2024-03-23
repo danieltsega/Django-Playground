64c4bc2e1a3fe7b7bbde195daf3fb85d4ae6ae40
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-24 01:37:04 +0300

    [Feature]: Add the logout url name

--- Authentication-Google/base/urls.py
+++ Authentication-Google/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, logout_view
 
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+    path("logout", logout_view, name="logout")
 ]
