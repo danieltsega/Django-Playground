c4ca77446fefcb4cce4bfa5d827e306b0ecd8147
DannySanchez6658 <danieltsega6658@gmail.com>
2024-03-26 21:24:10 +0300

    [Feature]: Safe guarding the home view via login decorator

--- Authentication-Google/base/views.py
+++ Authentication-Google/base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
