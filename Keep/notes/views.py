30ccd5fd16209c83ee18c3371f19c7177663de9d
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-02 01:49:48 +0300

    [Feature]: Add the class based views for the notes app based on authentication

--- Keep/notes/views.py
+++ Keep/notes/views.py
@@ -1,5 +1,39 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from .models import Note
+from .forms import NoteForm
+
+from django.urls import reverse_lazy
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class ListNoteView(LoginRequiredMixin, ListView):
+    model = Note
+    template_name = 'index.html'
+    
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+    
+class CreateNoteView(LoginRequiredMixin, CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = 'add_note.html'
+    success_url = reverse_lazy('home')
+    
+class DeleteNoteView(LoginRequiredMixin, DeleteView):
+    model = Note
+    success_url = reverse_lazy('home')
+    template_name = "index.html"
+
+class UpdateNoteView(LoginRequiredMixin, UpdateView):
+    model = Note
+    form_class = NoteForm
+    template_name = "add_note.html"
+    success_url = reverse_lazy("home")
+    
+class DetailNoteView(LoginRequiredMixin, DetailView):
+    model = Note
+    template_name = "detail_note.html"
