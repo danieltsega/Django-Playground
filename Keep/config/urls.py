715bb87ab14fe5b9a2ee5c1617098a11453c55ac
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-01 15:53:46 +0300

    [Feature]: Adding the allauth url

--- Keep/config/urls.py
+++ Keep/config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notes.urls')),
     path('todos/', include('todo.urls')),
+    path("accounts/", include('allauth.urls')),
 ]
