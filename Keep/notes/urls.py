fcd9e7ed412ee23027943f81f6048c615a1e8dd4
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-02 01:48:57 +0300

    [Feature]: Add the rest of the notes url

--- Keep/notes/urls.py
+++ Keep/notes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import CreateNoteView, DeleteNoteView, DetailNoteView, ListNoteView, UpdateNoteView
 
 urlpatterns = [
-    path('', index, name="home"),
+    path('', ListNoteView.as_view(), name="home"),
+    path("add-note", CreateNoteView.as_view(), name="add-note"),
+    path("delete-note/<int:pk>/", DeleteNoteView.as_view(), name="delete-note"),
+    path("edit-note/<int:pk>", UpdateNoteView.as_view(), name="edit-note"),
+    path("detail-note/<int:pk>", DetailNoteView.as_view(), name="detail-note")
 ]
