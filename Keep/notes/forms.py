b28a696d3a762a1f214cdccbd1a2ee36645804da
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-02 14:48:05 +0300

    [Fix]: Fixed the input background colors

--- Keep/notes/forms.py
+++ Keep/notes/forms.py
@@ -4,11 +4,11 @@ from .models import Note
 
 class NoteForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'h-10 w-full border rounded focus:outline-none py-3 px-3 text-gray-700  mr-2 bg-slate-200',
+        'class': 'h-10 w-full border rounded focus:outline-none py-3 px-3 text-gray-700  mr-2',
         'placeholder': 'Add your note title here...'
     }), label='')
     description = forms.CharField(widget=forms.Textarea(attrs={
-        'class': 'h-64 w-full border rounded focus:outline-none py-3 px-3 text-gray-700 mt-2 bg-slate-200',
+        'class': 'h-64 w-full border rounded focus:outline-none py-3 px-3 text-gray-700 mt-2',
         'placeholder': 'Write your note here...'
     }), label='')
     
