d8b80cab71a8664d27abc80a388be599be06723a
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-05 00:47:31 +0300

    [Feature]: Add the update, destroy and retreive url

--- REST-Intro/api/urls.py
+++ REST-Intro/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostListCreateView
+from .views import PostListCreateView, PostDetailRetrieveDestroyAPIView, PostUpdateAPIView
 
 urlpatterns = [
-    path('', PostListCreateView.as_view(), name='posts')
+    path('', PostListCreateView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailRetrieveDestroyAPIView.as_view(), name='retrieve'),
+    path('post/update/<int:pk>', PostUpdateAPIView.as_view(), name='update')
 ]
