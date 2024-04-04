4706dd8939f7fe747a87998987c581b03e2ce690
DannySanchez6658 <danieltsega6658@gmail.com>
2024-04-04 22:17:09 +0300

    [Feature]: Add the apps in the setting

--- REST-Intro/config/settings.py
+++ REST-Intro/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #local and other apps
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
